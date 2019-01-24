b9896a7b1b705447d849441cde3bdb59670311f3
Yashwant Kudalkar <yashwantk@transerv.local>
2019-01-24 13:07:02 +0530

    change

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,8 +6,7 @@ var cacheName = 'cache-v5';
 //Files to save in cache
 var files = [
   './',
-  './index.html?utm=homescreen', //SW treats query string as new request
-  './js/main.js',
+  './?utm=homescreen', //SW treats query string as new request
   './manifest.json'
 ];
 
@@ -166,7 +165,7 @@ self.addEventListener('sync', (event) => {
 
 //Adding `notification` click event listener
 self.addEventListener('notificationclick', (event) => {
-  var url = 'https://demopwa.in/';
+  var url = 'https://sss.yashwantkudalkar.com/';
 
   //Listen to custom action buttons in push notification
   if (event.action === 'yes') {
@@ -203,31 +202,3 @@ self.addEventListener('notificationclick', (event) => {
   );
 });
 
-let deferredPrompt;
-const addBtn = window.document.querySelector('.add-button');
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-});
